5a73ea50ad470cbfb00407c6cd3f417acfd96b35
ilvpap <85211001+ilvpap@users.noreply.github.com>
2025-05-09 20:34:21 +0900

    feat: All Scripts, All developer File 추가
     All Scripts, All developer File 추가

--- Assets/Scripts/Obstacle/Obstacle.cs
+++ Assets/Scripts/Obstacle/Obstacle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour
+public class Obstacle : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
