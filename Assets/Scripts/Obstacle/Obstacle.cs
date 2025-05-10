44faf042043c1a3e99e8e6b1a7ba64078fb8ef44
Juwon471 <asd2486216@naver.com>
2025-05-10 15:19:07 +0900

    feat: 장애물 및 스크립트추가
    Obstacle 랜덤위치 생성 그리고 장애물 사이 간격 조절

--- Assets/Scripts/Obstacle/Obstacle.cs
+++ Assets/Scripts/Obstacle/Obstacle.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float highPosY = 1f;
+    public float lowPosY = -1f;
 
-    // Update is called once per frame
-    void Update()
+    public float holeSizeMin = 1f;
+    public float holeSizeMax = 3f;
+
+    public float widthPaddingMin = 3f;
+    public float widthPaddingMax = 6f;
+
+    public Transform topObject;
+    public Transform bottomObject;
+    public Transform top2Object;
+
+    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount) 
     {
+        float holeSize = Random.Range(holeSizeMin, holeSizeMax);
+        float halfHoleSize = holeSize / 2f;
+
+        topObject.localPosition = new Vector3( 0 , halfHoleSize);
+        bottomObject.localPosition = new Vector3( 0, halfHoleSize);
+        top2Object.localPosition = new Vector3( 0, halfHoleSize);
         
+        float randomPadding = Random.Range(widthPaddingMin, widthPaddingMax);
+        Vector3 placePosition = lastPosition + new Vector3(randomPadding, 0);
+        placePosition.y = Random.Range(lowPosY, highPosY);
+
+        transform.position = placePosition;
+
+        return placePosition;
     }
 }
